c85116fa980af2ac84223f035a49f6851770f9e1
brimpf <150439988+brimpf@users.noreply.github.com>
2023-11-11 14:34:24 +0100

    created working setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsss_homework2',
+    version='1.0',
+    packages=['math_quiz'],
+    url='https://github.com/brimpf/dsss_homework2',
+    license='',
+    author='Brimpf',
+    author_email='',
+    description='Dsss homework 2'
+)
